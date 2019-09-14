1816f88a3e3de95cc38c9296248017cb9c9aecda
ItzZed <bellzach8@gmail.com>
2019-09-14 16:55:16 -0600

    Added Bunch of Stuff
    Littered with bugs. Didn't finish on time for CGJ so adding this. Will be finishing game.

---
+++ CGJ/Assets/Scripts/enemyController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyController : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+
+    private Rigidbody2D rb;
+
+    void Start() {
+
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update() {
+        
+    }
+
+}
---
+++ CGJ/Assets/Scripts/levelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelManager : MonoBehaviour {
+
+    void Start() {
+
+
+
+    }
+
+    void Update() {
+
+        if(Input.GetKeyDown(KeyCode.R)) {
+
+            loadLevel("Main");
+
+        }
+
+    }
+
+    public void loadLevel(string levelName) {
+
+        SceneManager.LoadScene(levelName);
+
+    }
+
+}
