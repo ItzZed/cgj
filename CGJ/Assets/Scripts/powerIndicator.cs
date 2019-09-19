067030d3328e075477805fe98d427faec08c1654
ItzZed <bellzach8@gmail.com>
2019-09-19 11:23:10 -0600

    Added Starter Scripts for the Power
    #13 - Added Starter Scripts for the Power
    + Scripts
    + UI Indicator

---
+++ CGJ/Assets/Scripts/powerIndicator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class powerIndicator : MonoBehaviour {
+
+    public Text indicator;
+    public realityPower rP;
+    
+
+    void Start() {
+
+        indicator = GetComponent<Text>();
+        
+    }
+
+    void Update() {
+
+        if(rP.powerOn == true) {
+
+            indicator.text = "On";
+
+        }
+        else if (rP.powerOn == false)
+        {
+
+            indicator.text = "Off";
+
+        }
+        else {
+
+            // If Displayed Something's Wrong
+            indicator.text = "powerIndicator String Error";
+
+        }
+
+    }
+}
---
+++ CGJ/Assets/Scripts/realityPower.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class realityPower : MonoBehaviour {
+
+    public bool powerOn;
+
+    void Start() {
+
+
+        
+    }
+
+    void Update() { 
+
+        if(Input.GetKey(KeyCode.Space)) {
+
+            powerOn = true;
+
+        }
+        else {
+
+            powerOn = false;
+
+        }
+        
+    }
+}
